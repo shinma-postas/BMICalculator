f7dc8538048167f9545540338d74c2fccf181195
Shinma Takahashi <shinma.takahashi@postas.co.jp>
2026-07-06 14:09:40 +0900

    Toolbar(Actionbar)実装

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -11,6 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // idを指定してツールバーを渡す。
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        // TextViewでタイトルを指定しているため、デフォルトのタイトルを非表示
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        // Rについて: プロジェクト内の画面パーツ、画像、文字、色などのリソース("R"esource)を呼び出すためのもの
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
