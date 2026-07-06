76c13fb08d76f675d14461149af6ed766ea5076d
Shinma Takahashi <shinma.takahashi@postas.co.jp>
2026-07-06 14:38:15 +0900

    ToolbarからConstraintLayoutに変更

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -12,12 +12,6 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // idを指定してツールバーを渡す。
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        // TextViewでタイトルを指定しているため、デフォルトのタイトルを非表示
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-
         // Rについて: プロジェクト内の画面パーツ、画像、文字、色などのリソース("R"esource)を呼び出すためのもの
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
