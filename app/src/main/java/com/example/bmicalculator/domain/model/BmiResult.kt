ae12c847853b57a497941acf40fd3d3b55eca173
Shinma Takahashi <shinma.takahashi@postas.co.jp>
2026-07-07 15:48:00 +0900

    modelを定義

---
+++ app/src/main/java/com/example/bmicalculator/domain/model/BmiResult.kt
@@ -0,0 +1,6 @@
+package com.example.bmicalculator.domain.model
+
+data class BmiResult(
+    val bmi: Double,
+    val category: String
+)
---
+++ app/src/main/java/com/example/bmicalculator/domain/model/HealthData.kt
@@ -0,0 +1,6 @@
+package com.example.bmicalculator.domain.model
+
+data class HealthData(
+    val heightCm: Double,
+    val weightKg: Double
+)
